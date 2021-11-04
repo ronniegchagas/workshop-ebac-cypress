d9739e60bf8c03f48def82c0fda1c5afc6c4b34b
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-11-04 15:28:38 -0300

    Adicionado testes de entrada, saída e exclusão

--- cypress/integration/devfinance.spec.js
+++ cypress/integration/devfinance.spec.js
@@ -1,20 +1,55 @@
 /// <reference types="cypress" />
 
 describe('DevFinance', () => {
-    // Criando um teste
-    it('Adicionar uma nova transão de entrada', () => {
-        // Ação de visitar a página
+    it('Adicionar uma nova transação de entrada', () => {
         cy.visit('https://devfinance-agilizei.netlify.app/');
 
-        // comandos get e contains
-        // get = mapeia um elemento para usar seu seletor
-        // contains = mapeia um elemento para usar seu texto
+        cy.get('a[onclick*=open]').click();
+        cy.get('input#description').type('Aplicativo de finanças');
+        cy.get('input#amount').type('10000');
+        cy.get('input#date').type('2021-11-03');
+        cy.contains('button', 'Salvar').click();
+
+        cy.get('table tbody tr').should('have.length', 1);
+    });
+
+    it('Adicionar uma nova transação de saída', () => {
+        cy.visit('https://devfinance-agilizei.netlify.app/');
+
+        cy.get('a[onclick*=open]').click();
+        cy.get('input#description').type('Kit Ryzen 7');
+        cy.get('input#amount').type('-8000');
+        cy.get('input#date').type('2021-11-04');
+        cy.contains('button', 'Salvar').click();
+
+        cy.get('table tbody tr').should('have.length', 1);
+    });
+
+    it('Remover uma transação de adicionada', () => {
+        cy.visit('https://devfinance-agilizei.netlify.app/');
+
         cy.get('a[onclick*=open]').click();
         cy.get('input#description').type('Freela');
         cy.get('input#amount').type('12');
         cy.get('input#date').type('2021-11-03');
         cy.contains('button', 'Salvar').click();
 
-        cy.get('table tbody tr').should('have.length', 1)
+        cy.get('table tbody tr').should('have.length', 1);
+
+        cy.get('table tbody tr:first-child img[onclick*=remove]').click();
+
+        cy.get('table tbody tr').should('have.length', 0);
     });
 });
+
+/*
+ * Comandos
+    describe = espeteste
+    it = novo teste
+    visit = visitar uma página específica
+    get = mapeia um elemento para usar seu seletor
+    contains = mapeia um elemento para usar seu texto
+    type = digitar algo no elemento selecionado
+    click = clicar em um elemento selecionado
+    should = deve ter uma especificação ...
+*/ 
